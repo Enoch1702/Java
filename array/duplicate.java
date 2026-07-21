527f51e392da56a3b06ad18fcbf722f9cb4be78f
Enoch1702 <enoch170206@gmail.com>
2026-07-21 22:42:54 +0530

    java DSA programs

--- array/duplicate.java
+++ array/duplicate.java
@@ -9,6 +9,7 @@ public class duplicate {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        
         System.out.println("Duplicate elements in the array are:");
         for(int i=0;i<n;i++){
             int flag=0;
