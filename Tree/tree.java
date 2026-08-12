055d99a04d24e755848ce318b9a5df11f69b9113
Enoch1702 <enoch170206@gmail.com>
2026-08-12 09:54:35 +0530

    Tree DSA

--- Tree/tree.java
+++ Tree/tree.java
@@ -1,3 +1,20 @@
+public class tree{
+    public static void main(String[] args) {
+        Tree t=new Tree();
+        t.root=new Node(10);
+        t.root.left=new Node(20);
+        t.root.right=new Node(30);
+        t.root.left.left=new Node(5);
+        System.out.print("Preorder : ");
+        t.preorder(t.root);
+        System.out.println();
+        System.out.print("Inorder : ");
+        t.inorder(t.root);
+        System.out.println();
+        System.out.print("Postorder : ");
+        t.postorder(t.root);
+    }
+}
 class Node{
     int data;
     Node left;
@@ -33,22 +50,5 @@ class Tree{
         postorder(root.left);
         postorder(root.right);
         System.out.print(root.data+" ");
-    }
-}
-public class Main{
-    public static void main(String[] args) {
-        Tree t=new Tree();
-        t.root=new Node(10);
-        t.root.left=new Node(20);
-        t.root.right=new Node(30);
-        t.root.left.left=new Node(5);
-        System.out.print("Preorder : ");
-        t.preorder(t.root);
-        System.out.println();
-        System.out.print("Inorder : ");
-        t.inorder(t.root);
-        System.out.println();
-        System.out.print("Postorder : ");
-        t.postorder(t.root);
     }
 }
